412ca9ed30a559dfceeaa8b463d6e60fb07c8db5
Jefry Jacky <jefryjacky@gmail.com>
2020-07-20 22:30:38 +0700

    pagination

--- app/src/main/java/p/com/githubapp/ui/searchusers/pagination/SearchUserPageKeyDataSourceFactory.kt
+++ app/src/main/java/p/com/githubapp/ui/searchusers/pagination/SearchUserPageKeyDataSourceFactory.kt
@@ -5,16 +5,16 @@ import androidx.paging.DataSource
 import io.reactivex.disposables.CompositeDisposable
 import p.com.githubapp.domain.entity.User
 import p.com.githubapp.domain.usecase.SearchUsersUseCase
+import javax.inject.Inject
 
-class SearchUserPageKeyDataSourceFactory constructor(
-    private val searchUsersUseCase: SearchUsersUseCase,
-    private val query:String,
-    private val disposeables:CompositeDisposable
+class SearchUserPageKeyDataSourceFactory @Inject constructor(
+    private val searchUsersUseCase: SearchUsersUseCase
 ): DataSource.Factory<Int, User>() {
-
+    var query:String? = null
+    var disposeables:CompositeDisposable? = null
     val sourceLiveData = MutableLiveData<SearchUserPageKeyDataSource>()
     override fun create(): DataSource<Int, User> {
-        val source = SearchUserPageKeyDataSource(searchUsersUseCase, query, disposeables)
+        val source = SearchUserPageKeyDataSource(searchUsersUseCase, query!!, disposeables!!)
         sourceLiveData.value = source
         return source
     }
---
+++ app/src/main/java/p/com/githubapp/ui/searchusers/pagination/SearchUsersPagination.kt
@@ -0,0 +1,50 @@
+package p.com.githubapp.ui.searchusers.pagination
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import io.reactivex.disposables.CompositeDisposable
+import p.com.githubapp.domain.entity.User
+import p.com.githubapp.ui.NetworkState
+import javax.inject.Inject
+
+class SearchUsersPagination @Inject constructor(
+    private val factory: SearchUserPageKeyDataSourceFactory
+) {
+    fun getDataSource(): LiveData<PagedList<User>> {
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setPrefetchDistance(10)
+            .build()
+        return LivePagedListBuilder(factory, config).build()
+    }
+
+    fun setDisposeable(disposables: CompositeDisposable){
+        factory.disposeables = disposables
+    }
+
+    fun setQuery(query: String){
+        factory.query = query
+    }
+
+    fun getNetworkState():LiveData<NetworkState>{
+        return Transformations.switchMap(factory.sourceLiveData){
+            it.networkStateEvent
+        }
+    }
+
+    fun getNoMatchingAccountEvent():LiveData<Boolean>{
+        return Transformations.switchMap(factory.sourceLiveData){
+            it.noMatchingAccountEvent
+        }
+    }
+
+    fun invalidate(){
+        factory.sourceLiveData.value?.invalidate()
+    }
+
+    fun retry(){
+        factory.sourceLiveData.value?.runRetry()
+    }
+}
