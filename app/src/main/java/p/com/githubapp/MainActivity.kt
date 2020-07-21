43d899a4417f5c2db3ec3418fb173f0fc60aa414
Jefry Jacky <jefryjacky@gmail.com>
2020-07-21 21:40:27 +0700

    Dagger inject ui

--- app/src/main/java/p/com/githubapp/MainActivity.kt
+++ app/src/main/java/p/com/githubapp/MainActivity.kt
@@ -2,9 +2,14 @@ package p.com.githubapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.Component
+import p.com.githubapp.di.ApplicationComponent
 
 class MainActivity : AppCompatActivity() {
+    lateinit var appComponent: ApplicationComponent
     override fun onCreate(savedInstanceState: Bundle?) {
+        appComponent = (application as GithubApp).appComponent
+        appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
--- app/src/main/java/p/com/githubapp/ui/searchusers/SearchUsersFragment.kt
+++ app/src/main/java/p/com/githubapp/ui/searchusers/SearchUsersFragment.kt
@@ -1,10 +1,12 @@
 package p.com.githubapp.ui.searchusers
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_search_users.*
+import p.com.githubapp.MainActivity
 import p.com.githubapp.R
 import p.com.githubapp.ui.BaseFragment
 import p.com.githubapp.ui.searchusers.adapter.UsersAdapter
@@ -13,6 +15,11 @@ class SearchUsersFragment: BaseFragment(R.layout.fragment_search_users) {
 
     private val mViewModel:SearchUsersViewModel by viewModels { viewModelFactory }
     private val mAdapter = UsersAdapter()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (activity as MainActivity).appComponent.inject(this)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
